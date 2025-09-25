7153d5083098251e32c65d0142f2d3135b7dc60b
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-09-25 09:20:25 -0300

    e4 - client register validation

---
+++ practico2/shop/src/components/clientPopUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-fade-transition>
+        <v-alert
+            title="¡Exito!"
+            type="success"
+            v-show="popUp"
+            class="position-fixed"
+        >
+            ¡Bienvenido/a {{client}}!
+
+        </v-alert>
+    </v-fade-transition>
+</template>
+
+<script setup>
+    import { defineProps, defineEmits, watch} from 'vue';
+    import { ALERT_DURATION } from '@/config/config';
+
+    const props = defineProps({
+        popUp: Boolean,
+        client: String
+    })
+
+
+    const emit = defineEmits(['update:popUp'])
+
+    function closePopUp()
+    {
+      emit('update:popUp', false)
+    }
+
+    watch(
+        () => props.popUp,
+        (val) => 
+        {
+            if (val) 
+            {
+                setTimeout(closePopUp, ALERT_DURATION)
+            }
+        }
+    )
+</script>
--- practico2/shop/src/pages/index.vue
+++ practico2/shop/src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Cart/>
+  <ClientInput/>
 </template>
 
 <script setup>

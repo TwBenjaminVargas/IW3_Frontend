0b3bd217dd1826ae8b24d5b8677e8c6a8240ce09
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-09-25 22:04:52 -0300

    add - cart drawer

--- practico2/shop/src/components/MyToolbar.vue
+++ practico2/shop/src/components/MyToolbar.vue
@@ -2,7 +2,7 @@
     <v-toolbar class="text-white bg-teal">
         
         <v-toolbar-title text="Shop!" class="text-h5"></v-toolbar-title>
-        <v-btn icon="mdi-cart-outline" size="x-large"></v-btn>
+        <v-btn icon="mdi-cart-outline" @click="drawer = !drawer" size="x-large"></v-btn>
         
         <template v-slot:extension>
             <v-tabs>
@@ -12,4 +12,17 @@
             </v-tabs>
         </template>
     </v-toolbar>
+    <v-slide-x-transition>
+        <Cart v-if="drawer"
+            class="position-fixed"
+            style="z-index: 100;"
+        >            
+        </Cart>
+    </v-slide-x-transition>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+    
+    const drawer = ref(false);
+</script>

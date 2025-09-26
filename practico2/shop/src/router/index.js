93c6a1d11ed4db95301a5882d1dc479b562fd9bd
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-09-26 01:02:34 -0300

    add - session management on router index

--- practico2/shop/src/router/index.js
+++ practico2/shop/src/router/index.js
@@ -5,32 +5,56 @@ import Products from '@/pages/Products.vue'
 import Clients from '@/pages/Clients.vue'
 import ProductDetail from '@/pages/ProductDetail.vue'
 import Login from '@/pages/Login.vue'
+
+import { sessionService } from '@/services/sessionService'
+
 const routes = [
   {
     path: '/products',       // URL
     name: 'Products',    // Name of route
-    component: Products  // Component
+    component: Products, // Component
+    meta: { requiresAuth: true } // requiresAuth
   },
   {
-    path: '/clients',       // URL
-    name: 'Clients',    // Name of route
-    component: Clients  // Component
+    path: '/clients',
+    name: 'Clients',
+    component: Clients,
+    meta: { requiresAuth: true }
   },
   {
-    path: '/products/:id',       // URL
-    name: 'Product Detail',    // Name of route
-    component: ProductDetail  // Component
+    path: '/products/:id',
+    name: 'Product Detail',
+    component: ProductDetail,
+    meta: { requiresAuth: true }
   },
   {
-    path: '/login',       // URL
-    name: 'Login',    // Name of route
-    component: Login  // Component
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(), // HTML5 History
   routes
+})
+
+// Guard global: checks session
+router.beforeEach((to, from, next) => {
+  const session = sessionService.isSessionOpen()
+
+  if (to.meta.requiresAuth && !session)
+  {
+    next({ name: 'Login' })
+  } 
+  else if (to.name === 'Login' && session)
+  {
+    next({ name: 'Home' })
+  }
+  else 
+  {
+    next()
+  }
 })
 
 export default router
